1d599ad3749c9e409ced247dd19ae7cce36e0d36
derekMgwena <derekmakhubela@gmail.com>
2022-11-29 02:04:45 +0200

    completed the app

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,45 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', 'f8bad06311msh398899cea0b798bp1b4e5cjsnb5143edddc13');
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+    getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
+    getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
+    getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+    getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+    getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
+    getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+  useGetSongsByGenreQuery,
+  useGetSongsByCountryQuery,
+  useGetSongsBySearchQuery,
+  useGetArtistDetailsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+} = shazamCoreApi;
+
+/*const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'f8bad06311msh398899cea0b798bp1b4e5cjsnb5143edddc13',
+    'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
+  }
+};
+
+fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));*/
